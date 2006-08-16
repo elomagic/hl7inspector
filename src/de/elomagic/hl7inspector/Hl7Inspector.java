443b04ffafd045b44c13b9c0bcfbf9b7d3c14404
Carsten Rambow <elomagic@users.noreply.github.com>
2006-08-16 17:29:14 +0000

    Version text changed

--- src/de/elomagic/hl7inspector/Hl7Inspector.java
+++ src/de/elomagic/hl7inspector/Hl7Inspector.java
@@ -127,5 +127,5 @@ public class Hl7Inspector {
     
     public static final String APPLICATION_NAME = "Hl7 Inspector";
     public static final String VERSION          = "2.2.0";
-    public static final String VERSION_LABEL    = "BETA";    
+    public static final String VERSION_LABEL    = "M1";    
 }
