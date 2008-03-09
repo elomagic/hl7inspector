95af9368a206d10ea048017bede5fc20f4779ee4
Carsten Rambow <elomagic@users.noreply.github.com>
2008-03-09 21:50:04 +0000

    GPL license text added

--- src/de/elomagic/hl7inspector/gui/LinkLabel.java
+++ src/de/elomagic/hl7inspector/gui/LinkLabel.java
@@ -1,10 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2008 Carsten Rambow
+ *
+ * Licensed under the GNU Public License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
+
 package de.elomagic.hl7inspector.gui;
 
-import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
