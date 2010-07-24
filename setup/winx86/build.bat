c1572a08313b7d38540d0b018e9136354478b0c5
Carsten Rambow <elomagic@users.noreply.github.com>
2010-07-24 12:39:05 +0000

    Unable to read version file. Fixed

--- setup/winx86/build.bat
+++ setup/winx86/build.bat
@@ -8,7 +8,7 @@ IF "%NSIS_FOLDER%"=="" (
 	EXIT /B
 )
 
-SET /p APP_VERSION=<version.txt
+SET /p APP_VERSION=<..\..\version.number
 
 IF "%APP_VERSION%"=="" (
 	ECHO Unable to determine version of HL7 Inspector
